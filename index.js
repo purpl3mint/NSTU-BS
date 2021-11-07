fe5e249b5ef8764fcc0d0a8bac4cca2a43a97059
Dmitry <purpl3mint@yandex.ru>
2021-11-07 14:28:51 +0700

    Changed file structure

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ const router = require('./routes/index')
 const errorHandler = require('./middleware/errorHandlingMiddleware')
 const path = require('path')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(fileUpload({}))
 app.use('/api', router)
 
